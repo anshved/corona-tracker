abfe8f61caf2344a42b575af6837a114916b3777
anshved <ansh.ved@sakec.ac.in>
2020-05-12 17:45:12 +0530

    new build

---
+++ docs/precache-manifest.21fac8407b291dba3dfa51fd1172f808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3f0aa3cfdb3a4ffebedb51e82f2f33",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "672faeca3e4fbb032724",
+    "url": "/corona-tracker/static/css/main.005b91fc.chunk.css"
+  },
+  {
+    "revision": "4c3fdc704437c1a59db0",
+    "url": "/corona-tracker/static/js/2.2a45ab96.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.2a45ab96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672faeca3e4fbb032724",
+    "url": "/corona-tracker/static/js/main.64e5402e.chunk.js"
+  },
+  {
+    "revision": "c04c614641dd3b1b9a32",
+    "url": "/corona-tracker/static/js/runtime-main.169def08.js"
+  }
+]);
